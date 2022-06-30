3aca327cbc34e42971457d56e58ff4db44dfe7d0
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2022-06-30 17:24:29 +0700

    tambah data untuk API listlogin

---
+++ api/users.js
@@ -0,0 +1,187 @@
+<<<<<<< HEAD:api/users.js
+const {createClient} = require('@supabase/supabase-js')
+
+//const SUPABASE_KEY = PROCESS.ENV.SUPABASE_KEY
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdGJ5dGFydnhsZnJrZ21mYXFxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NjI1NDE5MiwiZXhwIjoxOTcxODMwMTkyfQ.wrpDg8IdI9Q_Fd0Qu3xh2BVmKY8SYl7khhwfJhViIVs"
+const SUPABASE_URL = "https://pdtbytarvxlfrkgmfaqq.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const add = async (req, res) => {
+    try { 
+        const {tax, poli, date, location, name, nik, address, handphone} = req.query
+        const { data, error } = await supabase
+            .from('users')
+            .insert([
+                { tax, poli, date, location, name, nik, address, handphone },
+        ])
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const hapus = async (req, res) => {
+    try { 
+        const {id} = req.query
+        const { data, error } = await supabase
+            .from('users')
+            .delete()
+            .eq('id', id)
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const list = async (req, res) => {
+    try { 
+        const { data, error } = await supabase
+            .from('users')
+            .select('*')
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const update = async (req, res) => {
+    try { 
+        const {tax, poli, date, location, name, nik, address, handphone, id} = req.query
+        const { data, error } = await supabase
+            .from('users')
+            .update({ tax, poli, date, location, name, nik, address, handphone })
+            .eq('id', id)
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+module.exports = {
+    add, hapus, list, update
+=======
+const {createClient} = require('@supabase/supabase-js')
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdGJ5dGFydnhsZnJrZ21mYXFxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NjI1NDE5MiwiZXhwIjoxOTcxODMwMTkyfQ.wrpDg8IdI9Q_Fd0Qu3xh2BVmKY8SYl7khhwfJhViIVs"
+const SUPABASE_URL = "https://pdtbytarvxlfrkgmfaqq.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const add = async (req, res) => {
+    try { 
+        const {tax, poli, date, location, name, nik, address, handphone} = req.query
+        const { data, error } = await supabase
+            .from('users')
+            .insert([
+                { tax, poli, date, location, name, nik, address, handphone },
+        ])
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const daftar = async (req, res) => {
+    try { 
+        const {email, password, role} = req.query
+        const { data, error } = await supabase
+            .from('login')
+            .insert([
+                { email, password, role },
+        ])
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const hapus = async (req, res) => {
+    try { 
+        const {id} = req.query
+        const { data, error } = await supabase
+            .from('users')
+            .delete()
+            .eq('id', id)
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const list = async (req, res) => {
+    try { 
+        const { data: users, error } = await supabase
+            .from('users')
+            .select('*')
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(users)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const listlogin = async (req, res) => {
+    try { 
+        const { data, error } = await supabase
+            .from('login')
+            .select('id, email, password, role')
+        if (error) {
+            return res.json(error)
+        }
+
+        res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+const login = async (req, res) => {
+    try { 
+        const {email, password, role} = req.query
+        const { user, error } = await supabase.auth.signIn({
+            email: 'someone@email.com',
+            password: 'OOrwKRJMYSAPLyhAMnBL'
+          })
+        
+        res.json(user)
+    } catch (error) {
+        return res.json(error)
+    }    
+}
+
+module.exports = {
+    add, daftar, hapus, list, listlogin, login
+>>>>>>> 8aebd91a3d2a29b48ca5c0928104041dfbe809d0:api/gabungan.js
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const gabungan = require('../api/gabungan');
+const users = require('../api/users');
+const userLogin = require('../api/user-login');
 
+<<<<<<< HEAD
+router.get('/pasien/add', users.add)
+router.get('/pasien/list', users.list)
+router.get('/pasien/update', users.update)
+router.get('/pasien/delete', users.hapus)
+
+router.get('/user/add', userLogin.add)
+router.get('/user/list', userLogin.list)
+router.get('/user/update', userLogin.update)
+router.get('/user/delete', userLogin.hapus)
+=======
 router.get('/pasien/tambah', gabungan.add)
 router.get('/pasien/list', gabungan.list)
 router.get('/pasien/listLogin', gabungan.listlogin)
@@ -9,5 +21,6 @@ router.get('/pasien/daftar', gabungan.daftar)
 router.get('/pasien/login', gabungan.login)
 router.get('/pasien/hapus', gabungan.hapus)
 
+>>>>>>> 8aebd91a3d2a29b48ca5c0928104041dfbe809d0
 
 module.exports = router
