4e86598ef966814263d6aadc62c9132ac06d284b
Ilhamokta74 <ilhamoktavian74@gmail.com>
2022-06-30 17:37:43 +0700

    Merge branch 'main' of https://github.com/Ilhamokta74/respon-backend

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const users = require('../api/users');
 const userLogin = require('../api/user-login');
 
-<<<<<<< HEAD
 router.get('/pasien/add', users.add)
 router.get('/pasien/list', users.list)
 router.get('/pasien/update', users.update)
@@ -13,14 +12,5 @@ router.get('/user/add', userLogin.add)
 router.get('/user/list', userLogin.list)
 router.get('/user/update', userLogin.update)
 router.get('/user/delete', userLogin.hapus)
-=======
-router.get('/pasien/tambah', gabungan.add)
-router.get('/pasien/list', gabungan.list)
-router.get('/pasien/listLogin', gabungan.listlogin)
-router.get('/pasien/daftar', gabungan.daftar)
-router.get('/pasien/login', gabungan.login)
-router.get('/pasien/hapus', gabungan.hapus)
-
->>>>>>> 8aebd91a3d2a29b48ca5c0928104041dfbe809d0
 
 module.exports = router
