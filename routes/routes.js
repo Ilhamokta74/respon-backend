f4f9a11712c1ee4363772463ee73040c70334198
Ilhamokta74 <ilhamoktavian74@gmail.com>
2022-06-30 23:47:37 +0700

    tambah puskesmas

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pasien = require('../api/pasien');
 const userLogin = require('../api/user-login');
+const puskesmas = require('../api/puskesmas')
 
 router.get('/pasien/add', pasien.add)
 router.get('/pasien/list', pasien.list)
@@ -12,5 +13,10 @@ router.get('/user/add', userLogin.add)
 router.get('/user/list', userLogin.list)
 router.get('/user/update', userLogin.update)
 router.get('/user/delete', userLogin.hapus)
+
+router.get('/puskesmas/add', puskesmas.add)
+router.get('/puskesmas/list', puskesmas.list)
+router.get('/puskesmas/update', puskesmas.update)
+router.get('/puskesmas/delete', puskesmas.hapus)
 
 module.exports = router
