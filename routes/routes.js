38f9c6a57ab55d6f79aa0a2ec9b9f4ff1cdda8f1
Ilhamokta74 <ilhamoktavian74@gmail.com>
2022-07-02 23:00:38 +0700

    add API admin

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const pasien = require('../api/pasien');
-const userLogin = require('../api/user-login');
+const staff = require('../api/staff');
 const puskesmas = require('../api/puskesmas')
+const admin = require('../api/admin')
 
 router.get('/pasien/add', pasien.add)
 router.get('/pasien/list', pasien.list)
 router.get('/pasien/update', pasien.update)
 router.get('/pasien/delete', pasien.hapus)
 
-router.get('/staff/add', userLogin.add)
-router.get('/staff/list', userLogin.list)
-router.get('/staff/update', userLogin.update)
-router.get('/staff/delete', userLogin.hapus)
+router.get('/staff/add', staff.add)
+router.get('/staff/list', staff.list)
+router.get('/staff/update', staff.update)
+router.get('/staff/delete', staff.hapus)
 
 router.get('/puskesmas/add', puskesmas.add)
 router.get('/puskesmas/list', puskesmas.list)
 router.get('/puskesmas/update', puskesmas.update)
 router.get('/puskesmas/delete', puskesmas.hapus)
+
+router.get('/admin/add', admin.add)
+router.get('/admin/list', admin.list)
+router.get('/admin/update', admin.update)
+router.get('/admin/delete', admin.hapus)
 
 module.exports = router
