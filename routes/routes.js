3cf4a1d665aaf420a85f43ff6e7d5ba686efd0fb
Ilhamokta74 <ilhamoktavian74@gmail.com>
2022-07-03 09:13:45 +0700

    fix to access with postman

--- routes/routes.js
+++ routes/routes.js
@@ -5,24 +5,28 @@ const staff = require('../api/staff');
 const puskesmas = require('../api/puskesmas')
 const admin = require('../api/admin')
 
+// PASIEN
 router.post('/pasien/add', pasien.add)
 router.get('/pasien/list', pasien.list)
 router.put('/pasien/update', pasien.update)
 router.delete('/pasien/delete', pasien.hapus)
 
+// STAFF
 router.post('/staff/add', staff.add)
 router.get('/staff/list', staff.list)
 router.put('/staff/update', staff.update)
 router.delete('/staff/delete', staff.hapus)
 
-router.get('/puskesmas/add', puskesmas.add)
+// PUSKESMAS
+router.post('/puskesmas/add', puskesmas.add)
 router.get('/puskesmas/list', puskesmas.list)
-router.get('/puskesmas/update', puskesmas.update)
-router.get('/puskesmas/delete', puskesmas.hapus)
+router.put('/puskesmas/update', puskesmas.update)
+router.delete('/puskesmas/delete', puskesmas.hapus)
 
-router.get('/admin/add', admin.add)
+// ADMIN
+router.post('/admin/add', admin.add)
 router.get('/admin/list', admin.list)
-router.get('/admin/update', admin.update)
-router.get('/admin/delete', admin.hapus)
+router.put('/admin/update', admin.update)
+router.delete('/admin/delete', admin.hapus)
 
 module.exports = router
